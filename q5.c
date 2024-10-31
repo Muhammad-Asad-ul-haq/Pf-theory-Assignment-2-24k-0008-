adc9276540c75a3b515baea56c323e58ce34b399
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2024-10-31 09:07:21 -0700

    q5.c

--- q5.c
+++ q5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Function to print the horizontal histogram
+
 void horizontal_histogram(int array[], int No_of_values) {
     printf("\nHorizontal Histogram:\n");
     int i, j;
@@ -13,12 +13,12 @@ void horizontal_histogram(int array[], int No_of_values) {
     }
 }
 
-// Function to print the vertical histogram
+
 void vertical_histogram(int array[], int No_of_values) {
     printf("\nVertical Histogram:\n");
     int maximum_height = 0;
 
-    // Finding the maximum value to determine the height of the histogram
+    
     int i;
     for (i = 0; i < No_of_values; i++) {
         if (array[i] > maximum_height) {
@@ -26,7 +26,7 @@ void vertical_histogram(int array[], int No_of_values) {
         }
     }
 
-    // Print the histogram from top to bottom
+    
     int height;
     for (height = maximum_height; height > 0; height--) {
         for (i = 0; i < No_of_values; i++) {
@@ -39,7 +39,7 @@ void vertical_histogram(int array[], int No_of_values) {
         printf("\n");
     }
 
-    // Printing the values at the bottom
+   
     for (i = 0; i < No_of_values; i++) {
         printf("%d ", array[i]);
     }
@@ -50,7 +50,7 @@ int main() {
     int array[100]; 
     int No_of_values; 
 
-    // Input handling
+    
     printf("Enter the number of values: ");
     scanf("%d", &No_of_values);
 
@@ -62,10 +62,10 @@ int main() {
         scanf("%d", &array[i]); 
     }
 
-    // Print the horizontal histogram
+    
     horizontal_histogram(array, No_of_values);
     
-    // Print the vertical histogram
+    
     vertical_histogram(array, No_of_values);
 
     return 0;
